6c1baf8c5b6bb690665fb825d09069cf18537340
Hoxtygen <hoxtygen@live.com>
2019-06-03 12:15:58 +0100

    Initial commit

---
+++ instagram-app/src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import './App.css';
+import dummyData from "./dummy-data";
+import PostContainer from "./components/PostContainer/PostContainer";
+
+
+export default class App extends Component {
+  constructor(props)  {
+    super(props);
+    this.state = {
+      data: dummyData
+    }
+  }
+  render() {
+    //console.log(this.state.data);
+    const { data } = this.state;
+    return (
+      <div className="App">
+        <PostContainer
+          {...data}
+         /> 
+      </div>
+    );
+  }
+}
---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PostContainer(props) {
+    console.log(props)
+    return (
+        <div className = "post-container">
+            <h2>Hello, how are you doing?</h2>
+        </div>
+    )
+}
