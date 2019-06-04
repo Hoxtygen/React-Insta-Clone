8a477fec26354082615af6ade9591f8602ec2b6d
Hoxtygen <hoxtygen@live.com>
2019-06-04 16:10:20 +0100

    Add new comment
    A user can add new comment to the existing ones and see it render on the page.

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,28 +1,76 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Comment from "./Comment";
 import PropTypes from "prop-types";
 
-export default function CommentSection({ comments }) {
-    console.log(comments)
-    return (
-        <div className="comment-container">
-            {
-                comments.map((comment, index) => {
-                    return <Comment
-                        key={index}
-                        text={comment.text}
-                        username={comment.username}
+export default class CommentSection extends Component{
+    //console.log(comments)
+    constructor(props)   {
+        super(props);
+        this.state  = {
+            comments: this.props,
+            text: "",
+            index: "",
+        }
+    }
+
+    handleChange = (event) => {
+        //console.log(event.target.value)
+        this.setState({
+            text: event.target.value,
+            index: event.target.value
+        })
+    }
+
+    addNewComment = (event, index) => {
+        event.preventDefault();
+        const newComment = {
+            index: 5,
+            text: this.state.text,
+            username: "Maduro"
+        }
+        const commentList = this.props.comments.push(newComment);
+        this.setState({
+            comments: commentList,
+            text: "",
+            index: ""
+        })
+       
+        
+    }
+
+
+
+
+    render ()   {
+        //console.log(this.state.comments)
+        const { comments } = this.props;
+        return (
+            <div className="comment-container">
+                {
+                    comments.map((comment, index) => {
+                        return <Comment
+                            key={index}
+                            text={comment.text}
+                            username={comment.username}
+                        />
+                    })
+                }
+                <div className="new-comment">
+                    <form action="" onSubmit = {this.addNewComment}>
+                        <input
+                        type="text"
+                        name="newComment"
+                        id="new-comment"
+                        placeholder="Add a comment...."
+                        onChange = {this.handleChange}
                     />
-                })
-            }
-            <div className="new-comment">
-                <form action="">
-                    <input type="text" name="newComment" id="new-comment" placeholder="Add a comment...." />
-                    {/*<button type="submit">Add Comment</button>*/}
-                </form>
+                        {/*<button type="submit">Add Comment</button>*/}
+                    </form>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
 
 
